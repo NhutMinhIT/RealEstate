79dad35f2dd0d155474ece59ca26333d466e1902
NhutMinhIT <nhutminh.it19@gmail.com>
2024-01-19 10:36:57 +0700

    onfig routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import HomePage from '@/pages/Home/HomePage'
+
+import Introduction from '@/app/introduction/page'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="w-screen h-screen relative">
-      <HomePage />
+      <Introduction />
     </main>
   )
 }
