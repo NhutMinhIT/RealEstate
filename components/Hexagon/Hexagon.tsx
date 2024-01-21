85547bff3c372993665024a12a833c0915b6e7d5
NhutMinhIT <nhutminh.it19@gmail.com>
2024-01-22 01:27:24 +0700

    Response hexagon sm lg screen

--- components/Hexagon/Hexagon.tsx
+++ components/Hexagon/Hexagon.tsx
@@ -1,35 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 const Hexagon = () => {
     return (
-        <div className='w-[1688px] h-[794px] max-w-[80%] mx-auto'>
-            <div className='flex relative justify-center gap-2 z-1 w-[100%]'>
-                <div className='relative w-[300px] h-[300px'>
+        <div className='w-full max-w-[1688px] mx-auto'>
+            <div className='flex relative justify-center gap-2 z-1 w-full'>
+                <div className='relative lg:w-[300px] lg:h-[300px] w-[20vw] h-[20vw]'>
                     <div className='absolute inset-0 bg-black clip-path-mypolygon'></div>
                 </div>
-                <div className='relative w-[300px] h-[300px]'>
+                <div className='relative lg:w-[300px] lg:h-[300px] w-[20vw] h-[20vw]'>
                     <div className='absolute inset-0 bg-black clip-path-mypolygon'></div>
+                </div>
+                <div className='relative lg:w-[300px]  lg:h-[300px] w-[20vw] h-[20vw]'>
+                    <div className='absolute inset-0 bg-black clip-path-mypolygon'></div>
+                </div>
+            </div>
+            <div className='flex relative justify-center gap-2 z-2 bottom-[4vw]'>
+                <div className='relative lg:w-[300px] lg:h-[300px] w-[20vw] h-[20vw]'>
+                    <div className='absolute inset-0 bg-black clip-path-mypolygon'></div>
+                </div>
+                <div className='relative lg:w-[300px] lg:h-[300px] w-[20vw] h-[20vw]'>
+                    <div className='absolute inset-0 bg-black clip-path-mypolygon'></div>
+                </div>
+            </div>
+            <div className='flex relative justify-center gap-2 bottom-[8vw]'>
+                <div className='relative lg:w-[300px] lg:h-[300px] w-[20vw] h-[20vw]'>
+                    <div className='absolute inset-0 bg-black clip-path-mypolygon'></div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-                </div>
-                <div className='relative w-[300px] h-[300px]'>
-                    <div className='absolute inset-0 bg-black clip-path-mypolygon'></div>
-                </div>
-            </div>
-            <div className='flex relative justify-center gap-2 z-2 bottom-[70px]'>
-                <div className='relative w-[300px] h-[300px]'>
-                    <div className='absolute inset-0 bg-black clip-path-mypolygon'></div>
-                </div>
-                <div className='relative w-[300px] h-[300px]'>
-                    <div className='absolute inset-0 bg-black clip-path-mypolygon'></div>
-                </div>
-            </div>
-            <div className='flex relative justify-center gap-2 bottom-[140px]'>
-                <div className='relative w-[300px] h-[300px] '>
-                    <div className='absolute inset-0 bg-black clip-path-mypolygon'></div>
-                </div>
-            </div>
-        </div >
-    )
-}
-
-export default Hexagon
+export default Hexagon;
